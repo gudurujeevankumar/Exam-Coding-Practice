df149d799970a419bc1094888946720182d9e53e
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-04 23:28:01 +0530

    Happy Sad Percentage after some days

---
+++ HappySad.py
@@ -0,0 +1,15 @@
+n = int(input())
+k = int(input())
+
+happy = n
+sad = 0
+
+for _ in range(k):
+    new_happy = (0.5 * sad + 0.3 * happy)
+    new_sad = (0.7 * happy + 0.5 * sad)
+
+    happy = int(new_happy)
+    sad = int(new_sad)
+
+print(happy)
+print(sad)
