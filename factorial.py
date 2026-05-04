1efae9812dae2526823a4d16844b5516a5b62505
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-04 16:21:17 +0530

    Factorial of a number with recursive funtion and with normal way

---
+++ factorial.py
@@ -0,0 +1,16 @@
+# n = int(input())
+# fact = 1
+
+# for i in range(1,n+1):
+#     fact *= i
+
+# print(fact)
+
+
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * fact(n-1)
+
+n = int(input())
+print(fact(n))
